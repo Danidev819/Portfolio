7fb91951045a1f20fd15627488976a3e40d72ae4
danipro_2007 <elprodaniel13@gmail.com>
2023-01-05 14:08:03 +0100

    Update Projects

--- src/workData.ts
+++ src/workData.ts
@@ -5,6 +5,18 @@ const projects = [
     description: "Minecraft client tutorial using Mixin.",
     repoLink: "https://github.com/Danidev819/Cliente-Minecraft",
   },
+  {
+    title: "Fox-HCF",
+    techTitle: "minecraft",
+    description: "Minecraft HCF Core.",
+    repoLink: "https://github.com/Danidev819/Fox-HCF",
+  },
+  {
+    title: "AdvancedBlocker",
+    techTitle: "minecraft",
+    description: "Minecraft command blocker for BungeeCord.",
+    repoLink: "https://github.com/Danidev819/AdvancedBlocker",
+  },
 ];
 
 export default projects;
